15316140ae52be5c57e9b65695e29cf9985359fd
Shyam Sundar <shyam.sundar@byjus.com>
2019-08-05 19:15:50 +0530

    Reverting to old multiple record update fix

--- lib/active_admin/sortable_tree/controller_actions.rb
+++ lib/active_admin/sortable_tree/controller_actions.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
+
 module ActiveAdmin::SortableTree
   module ControllerActions
-
     attr_accessor :sortable_options
 
     def sortable(options = {})
-      options.reverse_merge! :sorting_attribute => :position,
-                             :parent_method => :parent,
-                             :children_method => :children,
-                             :roots_method => :roots,
-                             :tree => false,
-                             :max_levels => 0,
-                             :protect_root => false,
-                             :collapsible => false, #hides +/- buttons
-                             :start_collapsed => false,
-                             :sortable => true
+      options.reverse_merge! sorting_attribute: :position,
+                             parent_method: :parent,
+                             children_method: :children,
+                             roots_method: :roots,
+                             tree: false,
+                             max_levels: 0,
+                             protect_root: false,
+                             collapsible: false, # hides +/- buttons
+                             start_collapsed: false,
+                             sortable: true
 
       # BAD BAD BAD FIXME: don't pollute original class
       @sortable_options = options
@@ -21,34 +22,30 @@ module ActiveAdmin::SortableTree
       # disable pagination
       config.paginate = false
 
-      collection_action :sort, :method => :post do
+      collection_action :sort, method: :post do
         resource_name = ActiveAdmin::SortableTree::Compatibility.normalized_resource_name(active_admin_config.resource_name)
 
-        records = []
-        params[resource_name].each_pair do |resource, parent_resource|
-          parent_resource = resource_class.find(parent_resource) rescue nil
-          records << [resource_class.find(resource), parent_resource]
-        end
+        records = resource_class.where(id: params[resource_name].flatten.uniq).index_by { |record| record.id.to_s }
 
         errors = []
         ActiveRecord::Base.transaction do
-          records.each_with_index do |(record, parent_record), position|
-            record.send "#{options[:sorting_attribute]}=", position
-            if options[:tree]
-              record.send "#{options[:parent_method]}=", parent_record
+          params[resource_name].each_with_index do |(resource, parent_resource), position|
+            if records[resource]
+              records[resource].send "#{options[:sorting_attribute]}=", position
+              if options[:tree]
+                records[resource].send "#{options[:parent_method]}=", records[parent_resource]
+              end
             end
-            errors << {record.id => record.errors} if !record.save
+            errors << { records[resource].id => records[resource].errors } unless records[resource].save
           end
         end
         if errors.empty?
           head 200
         else
-          render json: errors, status: 422
+          render json: { error: { message: errors.map { |e| "#{e.keys.first} - #{e.values.first.full_messages.to_sentence}" }.join("\n") } }, status: 422
         end
       end
-
     end
-
   end
 
   ::ActiveAdmin::ResourceDSL.send(:include, ControllerActions)
